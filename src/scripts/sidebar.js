55dcfac6ee093ce4f732fb73a924c79399f2c5f9
B3none <ablackham2000@gmail.com>
2019-03-31 17:43:21 +0100

    Updated webpack config. Included react.

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,13 +1,22 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
 const divId = 'gdprconsent';
 
-class Sidebar {
+class Sidebar extends Component {
   static get divId() {
     return divId;
   }
 
   render() {
-
+    return(
+      <div>
+        <p>Test</p>
+      </div>
+    );
   }
 }
+
+ReactDOM.render(<Sidebar />, document.getElementById(divId));
 
 export default Sidebar;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   mode: 'production',
   entry: './gdprconsent.js',
   output: {
     filename: 'gdprconsent.min.js',
     path: path.resolve(__dirname, './build/scripts')
   },
-  context: path.resolve(__dirname, './src/scripts')
+  context: path.resolve(__dirname, './src/scripts'),
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  }
 };
